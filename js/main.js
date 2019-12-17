94a50a79bc6fd12aef8ec46e1d5c8fde95dd28ce
Safo <s.i.post@mai.ru>
2019-12-17 15:56:13 +0500

    feat: swiper for project

--- js/main.js
+++ js/main.js
@@ -30,6 +30,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 */
 
 
+
+
 $(document).ready(function() {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
@@ -41,6 +43,25 @@ $(document).ready(function() {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 19.5 + bullets.width() + 19.5)
+  bullets.css('left', prev.width() + 19.5)
 
 });
 
