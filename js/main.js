c8753090ea05bef3255f250709e0e2e8be830b45
Safo <s.i.post@mai.ru>
2019-12-23 18:02:33 +0500

    Feat: add yandex map JavaScript API

--- js/main.js
+++ js/main.js
@@ -200,4 +200,38 @@ $(document).ready(function() {
     return false;
   });
 
+  // Создаём карту с собственной меткой
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+      center: [47.244729, 39.723187],
+      zoom: 17
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Наш офис',
+        balloonContent: 'Вход со двора'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: '../img/map-marker.png',
+        // Размеры метки.
+        iconImageSize: [32, 32],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+      });
+
+    myMap.geoObjects
+      .add(myPlacemark);
+  });
+
 });
