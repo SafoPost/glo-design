37dd818b58fbdb1c40c310bbc2a9046c0f76c9d9
Safo <s.i.post@mai.ru>
2020-04-10 15:09:33 +0500

    refact adaptiv for fantasies, steps, clients

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,41 +1,11 @@
-/*
-document.addEventListener("DOMContentLoaded", function (event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  } 
-  
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal); 
-  });
-
-  closeBtn.addEventListener('click', switchModal);
-
-  document.onclick = function (event) {
-    if (event.target == modal) {
-      modal.classList.toggle('modal--visible');
-    }
-  };
-  
-  document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 27) {
-      modal.classList.remove('modal--visible');
-    }
-  });
-
-});
-*/
-$(document).ready(function() {
+$(document).ready(function () {
   var modal = $('.modal'),
-      modalBtn = $('[data-toggle=modal]'),
-      closeBtn = $('.modal__close');
-      send = $('.send'),
-      sendBtn = $('[data-toggle=modal-send]'),
-      closeSendBtn = $('.send__close');
-   
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close'),
+    send = $('.send'),
+    sendBtn = $('[data-toggle=modal-send]'),
+    closeSendBtn = $('.send__close');
+
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
@@ -43,18 +13,20 @@ $(document).ready(function() {
     modal.toggleClass('modal--visible');
   });
   closeSendBtn.on('click', function () {
-    send.toggleClass('send--visible');
+    send.removeClass('send--visible');
   });
 
-  var mySwiper = new Swiper('.swiper-container', {
+  // Слайдер-проекты
+  var mySwiper = new Swiper('.projects__swiper-container', {
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.projects__swiper-pagination',
       type: 'bullets',
+      clickable: true
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+      nextEl: '.projects__swiper-button-next',
+      prevEl: '.projects__swiper-button-prev'
     }
   });
 
@@ -64,33 +36,85 @@ $(document).ready(function() {
   var nextMobile = $('.swiper-button-next-mobile');
   var prevMobile = $('.swiper-button-prev-mobile');
   var bulletsMobile = $('.swiper-pagination-mobile');
-  // var cotainerMobile = $('.swiper-container');
-  // var navMobile = $('.swiper-navigation-mobile');
 
-  nextDesktop.css('left', prevDesktop.width() + 19.5 + bulletsDesktop.width() + 19.5),
-  bulletsDesktop.css('left', prevDesktop.width() + 19.5),
+  nextDesktop.css('left', prevDesktop.width() + 19 + bulletsDesktop.width() + 19);
+  bulletsDesktop.css('left', prevDesktop.width() + 19);
 
-  prevMobile.css('left', 68),
-  bulletsMobile.css('left', 130),
-  nextMobile.css('left', 232),
+  prevMobile.css('left', 68);
+  bulletsMobile.css('left', 130);
+  nextMobile.css('left', 232);
 
-  nextDesktop.css('left', prevDesktop.width() + 19.5 + bulletsDesktop.width() + 19.5),
-  bulletsDesktop.css('left', prevDesktop.width() + 19.5),
+  // Слайдер-шаги
+  var mySwiperSteps = new Swiper('.steps__swiper-container', {
+    loop: true,
+    speed: 1200,
+    effect: 'flip',
+    on: {
+      slideChange: function () {
+        let e = mySwiperSteps[0].activeIndex - 1;
+        if (e === 6) { e = 0 }
+        $('.steps__text--right').removeClass('steps__text--right-active');
+        $('.steps__text--right').eq(e).addClass('steps__text--right-active');
+      }
+    },
+    pagination: {
+      el: '.steps__swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.steps__swiper-button-next',
+      prevEl: '.steps__swiper-button-prev'
+    }
+  });
 
-  // cotainerMobile.css("position", "relative"),
-  // navMobile.css("position", "absolute"),
+  $('.steps__text--right').on('click', function () {
+    $('.steps__text--right').removeClass('steps__text--right-active');
+    $(this).addClass('steps__text--right-active');
+    const e = $(this).data('index');
+    mySwiperSteps[0].slideTo(e);
+    mySwiperSteps[1].slideTo(e);
+  });
 
-  // nextMobile.css('left', prevMobile.width() + 19.5 + bulletsMobile.width() + 19.5),
-  // bulletsMobile.css('left', prevMobile.width() + 19.5),
+  var nextSteps = $('.steps__swiper-button-next');
+  var prevSteps = $('.steps__swiper-button-prev');
+  var bulletsSteps = $('.steps__swiper-pagination');
 
-  // jQuery.fn.center = function () {
-  //   navMobile.css("position", "absolute");
-  //   navMobile.css("left", Math.max(0, (($(cotainerMobile).width() - $(navMobile).outerWidth()) / 2) +
-  //     $(cotainerMobile).scrollLeft()) + "px");
-  //   return this;
-  // };
+  nextSteps.css('left', prevSteps.width() + 18 + bulletsSteps.width() + 18);
+  bulletsSteps.css('left', prevSteps.width() + 18);
 
-  // $(navMobile).center();
+  // Секция-фантазии
+  $('.fantasies__slyles__items').on('click', function () {
+    $('.fantasies__slyles__items').removeClass('fantasies__slyles--active');
+    $(this).addClass('fantasies__slyles--active');
+    const e = $(this).data('index');
+
+    $('.fantasies__background__image').css('backgroundImage', `url('../img/fantasies/fantasies-${e}.jpg')`);
+
+    $('.fantasies__gallery__items__one').css('backgroundImage', `url('../img/fantasies/fantasies-${e}-a.jpg')`);
+    $('.fantasies__gallery__items__two').css('backgroundImage', `url('../img/fantasies/fantasies-${e}-b.jpg')`);
+    $('.fantasies__gallery__items__three').css('backgroundImage', `url('../img/fantasies/fantasies-${e}-c.jpg')`);
+    $('.fantasies__gallery__items__four').css('backgroundImage', `url('../img/fantasies/fantasies-${e}-d.jpg')`);
+  });
+
+  $('.fantasies__nav__prev').on('click', function () {
+    let e = $('.fantasies__slyles--active').data('index') - 1;
+    if (e === 0) { e = 11 }
+    $('.fantasies__background__image').css('backgroundImage', `url('../img/fantasies/fantasies-${e}.jpg')`);
+    e--;
+    if (e === 12) { e = 1 }
+    $('.fantasies__slyles__items').removeClass('fantasies__slyles--active');
+    $('.fantasies__slyles__items').eq(e).addClass('fantasies__slyles--active');
+  });
+  $('.fantasies__nav__next').on('click', function () {
+    let e = $('.fantasies__slyles--active').data('index') + 1;
+    if (e === 12) { e = 1 }
+    $('.fantasies__background__image').css('backgroundImage', `url('../img/fantasies/fantasies-${e}.jpg')`);
+    e--;
+    if (e === 12) { e = 1 }
+    $('.fantasies__slyles__items').removeClass('fantasies__slyles--active');
+    $('.fantasies__slyles__items').eq(e).addClass('fantasies__slyles--active');
+  });
 
   new WOW().init();
 
@@ -136,24 +160,83 @@ $(document).ready(function() {
       error.insertAfter($(policy));
     },
     // ajax .control__form
-    submitHandler: function(form) {
+    submitHandler: function (form) {
       $.ajax({
         type: "POST",
         url: "send.php",
         data: $(form).serialize(),
-        success: function(response) {
+        success: function (response) {
           console.log('Ajax сработал: ' + response);
           $(form)[0].reset();
           $(send).toggleClass('send--visible');
           $(".send__title").text(response);
         },
-        error: function(response) {
+        error: function (response) {
           console.log('Ajax не сработал: ' + response);
         }
       });
     }
   });
-  
+
+  $('#sample-form').validate({
+    errorClass: "invalid",
+    errorElement: "div",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: {
+        required: true,
+        minlength: 10
+      },
+      policy: {
+        required: true
+      }
+    },
+    // Сообщения .sample__form
+    messages: {
+      userName: {
+        required: "Назовите своё имя",
+        minlength: "Имя не должно быть короче двух символов",
+        maxlength: "Имя не должно быть длинее 15 символов"
+      },
+      userPhone: {
+        required: "Назовите свой телефон",
+        minlength: "Номер должен быть из 10 цифр"
+      },
+      policy: {
+        required: "Для отправки формы нужно согласиться с условиями"
+      }
+    },
+    // Проверка на чекнутость
+    errorPlacement: function (error, policy) {
+      if (policy.attr("type") == "checkbox") {
+        return policy.next('label').append(error);
+      }
+
+      error.insertAfter($(policy));
+    },
+    // ajax .sample__form
+    submitHandler: function (form) {
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          console.log('Ajax сработал: ' + response);
+          $(form)[0].reset();
+          $(send).toggleClass('send--visible');
+          $(".send__title").text(response);
+        },
+        error: function (response) {
+          console.log('Ajax не сработал: ' + response);
+        }
+      });
+    }
+  });
+
   $('#footer-form').validate({
     errorClass: "invalid",
     errorElement: "div",
@@ -221,7 +304,7 @@ $(document).ready(function() {
       });
     }
   });
-    
+
   $('#modal-form').validate({
     errorClass: "invalid",
     errorElement: "div",
@@ -254,7 +337,7 @@ $(document).ready(function() {
         required: "Назовите свой телефон",
         minlength: "Номер должен быть из 10 цифр"
       },
-      userEmail:{
+      userEmail: {
         required: "Укажите свой Email",
         email: "Введите в формате: name@domain.com"
       },
@@ -327,6 +410,7 @@ $(document).ready(function() {
     event.target.playVideo();
   }
 
+  // Отложенная загрузка карт
   YaMapsShown = false;
   YaMapsMinShown = false;
 
@@ -345,7 +429,7 @@ $(document).ready(function() {
     script.src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac5bb57cf85273372e734f7def62f256953ad064eedb76e5e5b9827bc18ec6a06&amp;width=100%25&amp;height=465&amp;lang=ru_RU&amp;scroll=false";
     document.getElementById("YaMaps").appendChild(script);
   };
-  
+
   $(window).scroll(function () {
     if (!YaMapsMinShown) {
       if ($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
